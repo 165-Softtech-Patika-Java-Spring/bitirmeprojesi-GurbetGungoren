4782954105fe2e62c58500480841e7094164d78e
Gurbet Gungoren <gurbet.gungoren@gmail.com>
2022-03-19 23:46:26 +0300

    add method findAllByProductType

---
+++ src/main/java/com/softtech/finalproject/gen/service/BaseEntityService.java
@@ -0,0 +1,45 @@
+package com.softtech.finalproject.gen.service;
+
+import com.softtech.finalproject.gen.enums.GenErrorMessage;
+import com.softtech.finalproject.gen.exceptions.ItemNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BaseEntityService<E, D extends JpaRepository> {
+    private final D dao;
+    public List<E> findAll(){
+        return dao.findAll();
+    }
+    public Optional<E> findById(Long id){
+        return dao.findById(id);
+    }
+    public E save(E entity) {
+        return (E)dao.save(entity);
+    }
+    public void delete(E entity){
+        dao.delete(entity);
+    }
+    public E getByIdWİthControl(Long id){
+        Optional<E> entityOptional = findById(id);
+        E entity;
+        if(entityOptional.isPresent()){
+            entity=entityOptional.get();
+        }
+        else{
+            throw  new ItemNotFoundException(GenErrorMessage.ITEM_NOT_FOUND);
+        }
+        return entity;
+    }
+    public boolean existsById(Long id){
+        return dao.existsById(id);
+    }
+    public D getDao() {
+        return dao;
+    }
+}
