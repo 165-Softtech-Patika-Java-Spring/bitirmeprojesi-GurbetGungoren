80bf39fe1e834dc027a84d7abc2bff39484b1231
Gurbet Gungoren <gurbet.gungoren@gmail.com>
2022-03-24 00:13:26 +0300

    handle exception

--- src/main/java/com/softtech/finalproject/gen/enums/GenErrorMessage.java
+++ src/main/java/com/softtech/finalproject/gen/enums/GenErrorMessage.java
@@ -1,13 +1,15 @@
 package com.softtech.finalproject.gen.enums;
 
 public enum GenErrorMessage implements BaseErrorMessage {
-    ITEM_NOT_FOUND("Item Not Found !"),
+    ITEM_NOT_FOUND("Item Not Found!"),
+    USERNAME_IS_USED("User name is used!"),
+    VALUE_CANNOT_BE_NEGATIVE("Value can not be negative or zero!"),
+    PARAMETER_CANNOT_BE_NULL("Parameter can not be null"),
     ;
     private String message;
     GenErrorMessage(String message) {
         this.message=message;
     }
-
     public String getMessage() {
         return message;
     }
